38da7db42681cd37e713facbad7c6e9d8c411a41
SliferGod <97369432+SliferGod@users.noreply.github.com>
2024-02-03 17:21:30 -0500

    Update VerletObject.cs
    tiny update

--- VerletObject.cs
+++ VerletObject.cs
@@ -3,14 +3,15 @@ using System;
 public class VerletObject {
     public Vec2 position;
     public Vec2 lastPosition;
+    public Vec2 acceleration;
 
     public void UpdatePosition(float dt)
     {
         //Calculate the velocity using the Verlet method
-        Vec2 velocity = new Vec2(position.x - lastPosition.x, position.y - lastPosition.y);
+        Vec2 velocity = position - lastPosition;
         //Save the current position as the old position since our position is about to change
-        lastPosition.setPosition(position);
+        lastPosition = position;
         //Perform Verlet Integration
-        
+        position = position + velocity + acceleration * dt * dt;
     }
 }
