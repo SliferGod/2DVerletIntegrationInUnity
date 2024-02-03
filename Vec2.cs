a262c3352f381da3233ca80da7a269f76b14cb01
SliferGod <97369432+SliferGod@users.noreply.github.com>
2024-02-03 17:32:35 -0500

    Update Vec2.cs
    Added operator overloads to apply to Vec2 and floats so that I can do things like, Vec2 -= airFriction or the like.

--- Vec2.cs
+++ Vec2.cs
@@ -52,6 +52,56 @@ public readonly struct Vec2 {
         else return false;
     }
 
+    public static Vec2 operator +(Vec2 a, float b)
+        => new Vec2(a.x + b, a.y + b);
+
+    public static Vec2 operator +(float a, Vec2 b)
+        => new Vec2(b.x + a, b.y + a);
+    
+    public static Vec2 operator -(Vec2 a, float b)
+        => a + (-b);
+
+    public static Vec2 operator -(float a, Vec2 b)
+        => a + (-b);
+
+    public static Vec2 operator *(Vec2 a, float b)
+        => new Vec2(a.x * b, a.y * b);
+
+    public static Vec2 operator *(float a, Vec2 b)
+        => new Vec2(a * b.x, a * b.y);
+
+    public static Vec2 operator /(Vec2 a, Vec2 b)
+    {
+        if (b.x == 0 || b.y == 0)
+        {
+            throw new DivideByZeroException();
+        }
+        return new Vec2(a.x / b.x, a.y / b.y);
+    }
+
+    public static Vec2 operator /(float a, Vec2 b)
+    {
+        if (b.x == 0 || b.y == 0)
+        {
+            throw new DivideByZeroException();
+        }
+        return new Vec2(a / b.x, a / b.y);
+    }
+
+    public static Vec2 operator /(Vec2 a, float b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException();
+        }
+        return new Vec2(a.x / b, a.y / b);
+    }
+
+    public static Vec2 operator ==(Vec2 a, Vec2 b) {
+        if(a.x == b.x && a.y == b.y) return true;
+        else return false;
+    }
+
     public static float distance(Vec2 a, Vec2 b) {
         return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
     }
