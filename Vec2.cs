5c31f4ac56bed25d6669cb3717ac195b9cb05630
SliferGod <97369432+SliferGod@users.noreply.github.com>
2024-02-03 17:38:55 +0000

    Basic start with two scripts

---
+++ Vec2.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Vec2 {
+    public float x;
+    public float y;
+
+    public Vec2() {
+        x = 0;
+        y = 0;
+    }
+
+    public Vec2(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ VerletObject.cs
@@ -0,0 +1,5 @@
+using System;
+
+public class VerletObject {
+    public Vec2 position;
+}
