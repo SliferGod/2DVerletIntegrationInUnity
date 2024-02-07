15f4a5ff1a79bfa1d1ed9e472c86232177b0fd5b
SliferGod <97369432+SliferGod@users.noreply.github.com>
2024-02-06 21:47:10 -0500

    Update PinnedLeash.cs
    finished up the pinned leash code

--- PinnedLeash.cs
+++ PinnedLeash.cs
@@ -4,12 +4,17 @@ using System.Collections.Generic;
 public class PinnedLeash : VerletConstraint {
   public &Vec2 pinPoint;
   public VerletObject obj;
+  public float leashDistance;
 
   public PinnedLeash(VerletObject obj) {
     this.obj = obj;
   }
   
   public void Constrain(List<VerletObject> objects, float dt) {
-    
+    float dist = Vec2.distance(pinPoint, obj.position);
+
+    if(dist > leashDistance) {
+      obj.position = new Vec2(leashDistance * (obj.position.x / dist), leashDistance * (obj.position.y / dist));
+    }
   }
 }
