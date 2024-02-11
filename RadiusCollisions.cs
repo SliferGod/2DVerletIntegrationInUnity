ecd13aeb575827da0e0a250c1b85259ce90aa163
SliferGod <97369432+SliferGod@users.noreply.github.com>
2024-02-11 15:10:11 -0500

    Update RadiusCollisions.cs
    added the functionality I think

--- RadiusCollisions.cs
+++ RadiusCollisions.cs
@@ -6,10 +6,16 @@ public RadiusCollisions : VerletConstraint {
   public override void Constrain(List<VerletObjects> objects, float dt) {
     for(int a = 0; a < objects.Count; a++) {
       for(int b = 0; b < objects.Count; b++) {
-        float dist = Vec2.distance(objects[a].position, objects[b].position);
+        float distBetween = Vec2.distance(objects[a].position, objects[b].position);
         
-        if(dist < objects[a].radius + objects[b].radius) {
-            
+        if(distBetween < objects[a].radius + objects[b].radius) {
+            VerletObject objOne = objects[a];
+            VerletObject objTwo = objects[b];
+          
+            Vec2 middlePoint = new Vec2((objOne.position.x + objTwo.position.x) / 2, (objOne.position.y + objTwo.position.y) / 2);
+            float dist = distBetween / 2f;
+            objOne.position = (.5f * (objOne.radius + objTwo.radius) * ((objOne.position - middlePoint)) / dist) + middlePoint;
+            objTwo.position = (.5f * (objOne.radius + objTwo.radius) * ((objTwo.position - middlePoint)) / dist) + middlePoint;
         }
       }
     }
