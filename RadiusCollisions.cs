0891dbcba905177907a8d22483396115af17ef30
SliferGod <97369432+SliferGod@users.noreply.github.com>
2024-02-11 14:17:33 -0500

    Update RadiusCollisions.cs
    added detection, still needs movement of objects.

--- RadiusCollisions.cs
+++ RadiusCollisions.cs
@@ -1,7 +1,18 @@
 using System;
-using System.Collections;
+using System.Collections.Generic;
 
 public RadiusCollisions : VerletConstraint {
-  
+
+  public override void Constrain(List<VerletObjects> objects, float dt) {
+    for(int a = 0; a < objects.Count; a++) {
+      for(int b = 0; b < objects.Count; b++) {
+        float dist = Vec2.distance(objects[a].position, objects[b].position);
+        
+        if(dist < objects[a].radius + objects[b].radius) {
+            
+        }
+      }
+    }
+  }
 
 }
